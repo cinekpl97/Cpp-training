4b341c07f3c36d7c32a9d3557c13ec7da4d17abf
cinekpl97 <cinekpl97@gmail.com>
2018-05-06 18:48:03 +0200

    First version, made thanks to pseudocode in book "Metody Numeryczne" Fortuna, Macukow, Wasowski. It might change when I find out what happens here...

--- Newton-Cotes Quadrature Rules/main.cpp
+++ Newton-Cotes Quadrature Rules/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
+double function(double value) {
+
+}
+
+double newtonCotesQuadrature(double a, double b, double amountOfNodes) {
+    double S = 5 * (function(a) + function(b));
+    double step = (b - a) / amountOfNodes;
+    double x = a + step;
+    double i = 1;
+    while (i != amountOfNodes) {
+        S = S + function(x);
+        x = x + step;
+        i = i + 1;
+    }
+    S = S + step;
+    return S;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
     return 0;
 }
