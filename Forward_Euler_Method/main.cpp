709104b3b9197efb159d0c320a5b128f56776ac7
cinekpl97 <cinekpl97@gmail.com>
2018-05-23 13:03:51 +0200

    Added everything, possible mistake in amountOfCounts, but works for this example

--- Forward_Euler_Method/main.cpp
+++ Forward_Euler_Method/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
+double dyDivideDx(double x) {
+    return 2 * x;
+}
+
+void forwardEulerMethod(double y[], double step, int N) {
+    for (int i = 0; i < N / step -1; i++) {
+        y[i + 1] = y[i] + step * dyDivideDx(i);
+    }
+    for (int i = 0; i < N / step; i++) {
+        cout << "x" << i << " = " << y[i] << endl;
+    }
+}
+
 int main() {
-    
+    int N = 5;
+    double step = 0.5;
+    double amountOfCounts = N / step;
+    auto intAmountOfCounts =(int)amountOfCounts;
+    auto *y = new double[intAmountOfCounts];
+    y[0] = 0;
+    forwardEulerMethod(y, step, N);
     return 0;
 }
