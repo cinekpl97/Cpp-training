ca972129c0f31b0a40c02a93d96739dc93d5dd82
cinekpl97 <cinekpl97@gmail.com>
2018-05-23 13:31:50 +0200

    Need to finish this one

---
+++ Richardson_Extrapolation_Method/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void richardsonExtrapolation() {
+    double yext =
+}
+
+int main() {            //to use Richardson Method you need to use other method ex. Euler, Crank-Nicholson
+
+
+    return 0;
+}
