7782256eb5b0528d28ffc0a2f9b873f0c1ff4066
cinekpl97 <cinekpl97@gmail.com>
2018-05-28 11:49:45 +0200

    Everything should work fine, but the example from lesson isnt quite accurate

--- Runge Kutty Forth Row/main.cpp
+++ Runge Kutty Forth Row/main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 double dydividedx(double x, double y) {
-    return y - x * x + 1;
+    return x+y;
 }
 
 double rungeKuttyForthRow(double y[], double N, double step) {
     auto k = new double[4];
     double deltaYn = 0;
-    for (int x = 0; x < N / step - 1; x++) {
+    for (int x = 0; x < N / step; x++) {
         k[0] = step * dydividedx(x, y[x]);
         k[1] = step * dydividedx(x + step / 2, y[x] + 0.5 * k[0]);
         k[2] = step * dydividedx(x + step / 2, y[x] + 0.5 * k[1]);
@@ -17,19 +17,19 @@ double rungeKuttyForthRow(double y[], double N, double step) {
         deltaYn = (1.0 / 6.0) * (k[0] + 2 * k[1] + 2 * k[2] + k[3]);
         y[x + 1] = y[x] + deltaYn;
     }
-    for (int i = 0; i < N / step - 1; i++) {
+    for (int i = 0; i < N / step; i++) {
         cout << "y" << i << " = " << y[i] << endl;
     }
 
 }
 
 int main() {
-    int N = 2;
-    double step = 0.5;
+    double N = 0.2;
+    double step = 0.1;
     double amountOfCounts = N / step;
     auto intAmountOfCounts = (int) amountOfCounts;
     auto *y = new double[intAmountOfCounts];
-    y[0] = 0.5;
+    y[0] = 1;
     rungeKuttyForthRow(y, N, step);
     return 0;
 }
