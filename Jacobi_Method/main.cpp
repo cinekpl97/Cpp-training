bdc1aca9e1d10de64ea623af71e44b1ca1cdfe21
cinekpl97 <cinekpl97@gmail.com>
2018-06-06 18:56:29 +0200

    Changed last counting, but results are the same

--- Jacobi_Method/main.cpp
+++ Jacobi_Method/main.cpp
@@ -2,25 +2,24 @@
 
 using namespace std;
 
-void Jacobi_Method(){
-    const int matrixSize = 4;
-    const int iterations = 1;
-    double A[][matrixSize] = {{4,   -1, 0.2, 2},
-                              {-1,  5,  0,   -3},
-                              {0.2, 1,  10,  -1},
-                              {0,   -2, -1,  4}
-    };
-    double b[] = {30, 0, -10, 5};
+void Jacobi_Method(int matrixSize) {
+    const int iteration = 3;
     double N[matrixSize];
     double M[matrixSize][matrixSize];
-    double x1[matrixSize], x2[matrixSize];
+    double x[matrixSize], xtemp[matrixSize];
+    double A[][matrixSize] = {{5,  -2, 3},
+                              {-3, 9,  1},
+                              {2,  -1, -7}
+    };
+    double b[] = {-1, 2, 3};
 
-// Calculate N = D^-1
+    for (int i = 0; i < matrixSize; i++) {
+        x[i] = 0;
+        xtemp[i] = 0;
+    }
     for (int i = 0; i < matrixSize; i++) {
         N[i] = 1 / A[i][i];
     }
-
-// Calculate M = -D^-1 (L + U)
     for (int i = 0; i < matrixSize; i++) {
         for (int j = 0; j < matrixSize; j++)
             if (i == j) {
@@ -29,7 +28,7 @@ void Jacobi_Method(){
                 M[i][j] = -(A[i][j] * N[i]);
             }
     }
-
+    cout << "M: " << endl;
     for (int i = 0; i < matrixSize; ++i) {
         for (int j = 0; j < matrixSize; j++) {
             cout << M[i][j] << ' ';
@@ -37,35 +36,44 @@ void Jacobi_Method(){
         cout << endl;
     }
     cout << endl;
+    cout << "N: " << endl;
     for (int i = 0; i < matrixSize; ++i) {
-        cout << N[i] << ' ';
+        cout << N[i] << endl;
     }
     cout << endl;
-
-//Initialize x
     for (int i = 0; i < matrixSize; i++) {
-        x1[i] = 0;
+        x[i] = 0;
+    }
+    for (int i = 0; i < matrixSize; i++) {
+        cout << xtemp[i] << endl;
     }
 
-    for (int k = 0; k < iterations; k++) {
-        for (int i = 0; i < matrixSize; i++) {
-            x2[i] = N[i] * b[i];
-            for (int j = 0; j < matrixSize; j++) {
-                x2[i] += M[i][j] * x1[j];
+    for (int i = 0; i < matrixSize; i++) {
+        xtemp[i] = N[i] * b[i];
+    }
+    for(int k=0; k<iteration; k++)
+    {
+        for(int i=0; i<matrixSize; i++)
+        {
+            xtemp[i] = N[i] * b[i];
+            for(int j=0; j<matrixSize; j++)
+            {
+                xtemp[i] += M[i][j] * x[j] ;
             }
         }
-        for (int i = 0; i < matrixSize; i++) {
-            x1[i] = x2[i];
+        cout << "Wyniki numer: " << k + 1 << endl;
+        for(int i=0; i<matrixSize; i++)
+        {
+            cout << "x" << i + 1 << " = " << x[i] << endl;
+            x[i] = xtemp[i];
         }
-    }
-
-    for (int i = 0; i < matrixSize; i++) {
-        cout << "x" << i + 1 << " = " << x1[i] << endl;
+        cout << endl;
     }
 }
 
-int main() {
 
+int main() {
+    Jacobi_Method(3);
 
     return 0;
 
